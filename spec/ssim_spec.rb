125539b9adf9da3012ead401ff0ec21d29b66635
Kolesnikov Danil <kolesnikovde@gmail.com>
2015-09-29 23:59:17 +0300

    Fixed specs.

--- spec/ssim_spec.rb
+++ spec/ssim_spec.rb
@@ -4,7 +4,7 @@ describe SSIM do
   URL = File.expand_path('../sample.ssim', __FILE__)
 
   describe '.schedule' do
-    it 'provides info about dates' do
+    it 'provides info about schedule' do
       schedule = SSIM.schedule(url: URL, date: Date.new(2014, 11, 7))
 
       expect(schedule.created_at).to eq(Date.new(2014, 11, 4))
@@ -12,63 +12,71 @@ describe SSIM do
       expect(schedule.to_date).to    eq(nil)
     end
 
-    it 'provides info about flights' do
+    it 'filters flights by date' do
       schedule = SSIM.schedule(url: URL, date: Date.new(2014, 11, 7))
-      table = schedule.table
 
-      expect(table['0423'].map(&:to_hash)).to eq([{
-        'aircraft_type' => 'A40',
-        'arrival_point' => 'BQJ',
-        'arrival_time' => DateTime.parse('10:30 +09:00'),
+      expect(schedule.table['0423'].map(&:to_hash)).to eq([{
+        'flight_number'          => '0423',
         'itinerary_variation_id' => '01',
-        'days_of_operations' => [ 5 ],
-        'departure_point' => 'YKS',
-        'departure_time' => DateTime.parse('08:30 +09:00'),
-        'flight_number' => '0423',
-        'from_date' => Date.new(2014, 11, 7),
-        'to_date' => Date.new(2014, 12, 26)
+        'from_date'              => Date.new(2014, 11, 7),
+        'to_date'                => Date.new(2014, 12, 26),
+        'days_of_operations'     => [5],
+        'departure_point'        => 'YKS',
+        'departure_time'         => Time.parse('08:30 +09:00'),
+        'arrival_point'          => 'BQJ',
+        'arrival_time'           => Time.parse('10:30 +09:00'),
+        'aircraft_type'          => 'A40',
       }])
 
-      expect(table['0424'].map(&:to_hash)).to eq([{
-        'aircraft_type' => 'A40',
-        'arrival_point' => 'YKS',
-        'arrival_time' => DateTime.parse('13:20 +09:00'),
+      expect(schedule.table['0424'].map(&:to_hash)).to eq([{
+        'flight_number'          => '0424',
         'itinerary_variation_id' => '01',
-        'days_of_operations' => [ 5 ],
-        'departure_point' => 'BQJ',
-        'departure_time' => DateTime.parse('11:20 +09:00'),
-        'flight_number' => '0424',
-        'from_date' => Date.new(2014, 11, 7),
-        'to_date' => Date.new(2014, 12, 26)
+        'from_date'              => Date.new(2014, 11, 7),
+        'to_date'                => Date.new(2014, 12, 26),
+        'days_of_operations'     => [5],
+        'departure_point'        => 'BQJ',
+        'departure_time'         => Time.parse('11:20 +09:00'),
+        'arrival_point'          => 'YKS',
+        'arrival_time'           => Time.parse('13:20 +09:00'),
+        'aircraft_type'          => 'A40',
       }])
     end
 
-    it 'groups flights with same itinerary variation id' do
-      schedule = SSIM.schedule(url: URL, date: Date.new(2014, 11, 7))
-      table = schedule.table
+    it 'filters flights by arrival point' do
+      schedule = SSIM.schedule(url: URL, date: Date.new(2014, 11, 7), arrival_point: 'BQJ')
+      expect(schedule.table.keys).to eq(['0423'])
+    end
 
-      expect(table['0755'].map(&:to_hash)).to eq([{
-        'flight_number' => '0755',
+    it 'filters flights by departure point' do
+      schedule = SSIM.schedule(url: URL, date: Date.new(2014, 11, 7), departure_point: 'YKS')
+      expect(schedule.table.keys).to eq(['0423', '0425'])
+    end
+
+    it 'groups flights with same itinerary variation id' do
+      schedule = SSIM.schedule(url: URL, date: Date.new(2014, 11, 13))
+
+      expect(schedule.table['0755'].map(&:to_hash)).to eq([{
+        'flight_number'          => '0755',
         'itinerary_variation_id' => '01',
-        'from_date' => Date.new(2014, 11, 13),
-        'to_date' => Date.new(2014, 11, 13),
-        'days_of_operations' => [ 4 ],
-        'departure_point' => 'KRR',
-        'departure_time' => DateTime.parse('23:25:00 +0900'),
-        'arrival_point' => 'OVB',
-        'arrival_time' => DateTime.parse('03:20:00 +0900'),
-        'aircraft_type' => '73W'
+        'from_date'              => Date.new(2014, 11, 13),
+        'to_date'                => Date.new(2014, 11, 13),
+        'days_of_operations'     => [ 4 ],
+        'departure_point'        => 'KRR',
+        'departure_time'         => Time.parse('23:25:00 +0900'),
+        'arrival_point'          => 'OVB',
+        'arrival_time'           => Time.parse('03:20:00 +0900'),
+        'aircraft_type'          => '73W',
       }, {
-        'flight_number' => '0755',
+        'flight_number'          => '0755',
         'itinerary_variation_id' => '01',
-        'from_date' => Date.new(2014, 11, 14),
-        'to_date' => Date.new(2014, 11, 14),
-        'days_of_operations' => [ 5 ],
-        'departure_point' => 'OVB',
-        'departure_time' => DateTime.parse('04:50:00 +0900'),
-        'arrival_point' => 'GDX',
-        'arrival_time' => DateTime.parse('10:00:00 +0900'),
-        'aircraft_type' => '73W'
+        'from_date'              => Date.new(2014, 11, 14),
+        'to_date'                => Date.new(2014, 11, 14),
+        'days_of_operations'     => [5],
+        'departure_point'        => 'OVB',
+        'departure_time'         => Time.parse('04:50:00 +0900'),
+        'arrival_point'          => 'GDX',
+        'arrival_time'           => Time.parse('10:00:00 +0900'),
+        'aircraft_type'          => '73W',
       }])
     end
   end
